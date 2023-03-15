2ee847abd32d9b8bc0e85983532e2f81e27d93e6
Muhammad Fatkhurrohman <hi@rohman.dev>
2023-03-15 12:06:00 +0700

    write main func

--- Calculator.cs
+++ Calculator.cs
@@ -2,7 +2,16 @@ class Calculator
 {
     static void Main(string[] args)
     {
+        
+        int a = 10;
+        int b = 6;
 
+        Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Add(a, b));
+        Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Sub(a, b));
+
+
+        Console.WriteLine("\nTekan sembarang key untuk keluar");
+        Console.ReadKey();
     }
 
     static int Add(int a, int b)
