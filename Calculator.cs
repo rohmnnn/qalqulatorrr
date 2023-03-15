68ac9c16d6fdbe6c51f80eb24a522e660cd4ce3a
Muhammad Fatkhurrohman <hi@rohman.dev>
2023-03-15 12:19:49 +0700

    add namespace

--- Calculator.cs
+++ Calculator.cs
@@ -1,26 +1,31 @@
-class Calculator
+using System;
+
+namespace CalculatorApp
 {
-    static void Main(string[] args)
+    class Calculator
     {
-        int a = 10;
-        int b = 6;
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 6;
 
-        Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Add(a, b));
-        Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Sub(a, b));
+            Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Add(a, b));
+            Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Sub(a, b));
 
 
-        Console.WriteLine("\nTekan sembarang key untuk keluar");
-        Console.ReadKey();
-    }
+            Console.WriteLine("\nTekan sembarang key untuk keluar");
+            Console.ReadKey();
+        }
 
-    static int Add(int a, int b)
-    {
-        return a + b;
-    }
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
 
-    static int Sub(int a, int b)
-    {
-        return a - b;
+        static int Sub(int a, int b)
+        {
+            return a - b;
+        }
     }
 }
 
